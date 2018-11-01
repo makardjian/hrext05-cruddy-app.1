1b940c59cc67019a695f27392ee858fd3fa85173
Michael Kardjian <makardjian@gmail.com>
2018-11-01 13:55:27 -0700

    almost complete writing the code for adding an item and using dynamic keys for local storage

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
-/*
-listen for click event (edit)
-update text in local storage (with key)
-update display with new text value
- */
+
+
+
+
 
 $(document).ready(function(){
-  console.log("before\n", window.localStorage);
 
-  // add event listener
-  $(".add-text-btn").on("click", function(){
-    $(".show-text").empty();
-    var curTextValue = $('#theKey').val(); // reading from <input>
-    var curKeyValue = "theKey"; // change to dynamic key?
-    localStorage.setItem(curKeyValue, curTextValue);
-    $(".show-text").append(curTextValue);
+  //variables
+  var list = document.querySelector('#list');
+  var addItem = document.querySelector('.add-item-btn');
+  var clearBtn = document.querySelector('.clear-checked-btn');
+  var inputText = document.querySelector('.text-input');
+  var storageKey = 0
+
+  //functions
+  var Grocery = function(groceryName) {
+    this.groceryName = groceryName;
+    this.createdAt = new Date();
+    this.complete = false;
+  }
+
+
+
+
+  // add an item to the list
+/*
+    Strategy:
+      /if the inputText.value.length is > 0:
+        -Concatenate a new List element together 
+        -appnd the new list item to 'list' which is equal to the ul div.
+        -put the new list item into localStorage
+        -
+
+    */
+
+  addItem.addEventListener('click',function(e){
+    if (inputText.value.length > 0) {
+      var currentItem = new Grocery(inputText.value)
+      localStorage.setItem(storageKey, currentItem.groceryName)
+      list.innerHTML += '<li>' + inputText.value + '</li>';
+      inputText.value = '';
+      storageKey++
+      console.log(localStorage)
+      // console.log(currentItem)
+    }
+
+
   });
 
   // remove item from app
