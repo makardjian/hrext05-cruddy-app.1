8662bb45a673f1cc97e7cb24f5517debda7964b5
Michael Kardjian <makardjian@gmail.com>
2018-11-01 18:26:50 -0700

    end of Thursday work

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-
+//NEED TO FIGURE OUT HOW TO KEEP GROCERY ITEMS ON THE PAGE WHEN THE PAGE RELOADS
 
 
 $(document).ready(function(){
@@ -9,8 +9,10 @@ $(document).ready(function(){
   var list = document.querySelector('#list');
   var addItem = document.querySelector('.add-item-btn');
   var clearBtn = document.querySelector('.clear-checked-btn');
+  var updateBtn = document.querySelector('.update-item-btn')
   var inputText = document.querySelector('.text-input');
   var storageKey = 0
+  var storageKeysForRemoval =[];
 
   //functions
   var Grocery = function(groceryName) {
@@ -24,7 +26,7 @@ $(document).ready(function(){
   addItem.addEventListener('click',function(e){
     if (inputText.value.length > 0) {
       var currentItem = new Grocery(inputText.value);
-      console.log(currentItem)
+      // console.log(currentItem)
       localStorage.setItem(storageKey, JSON.stringify(currentItem));
       list.innerHTML += '<li>' + inputText.value + '</li>';
       inputText.value = '';
@@ -36,23 +38,52 @@ $(document).ready(function(){
   });
 
 
-  //check a grocery off the list
-  list.addEventListener('click', function(e) {
-    var clickedItem = event.target;
-    $(clickedItem).addClass('complete')
-    console.log(event.target)
-  })
+  //add an item by pressing 'Enter' on keyboard instead of clicking 'Add Item' button
+    inputText.addEventListener('keydown',function(e){
+    if (inputText.value.length > 0 && e.which === 13) {
+      var currentItem = new Grocery(inputText.value);
+      // console.log(currentItem)
+      localStorage.setItem(inputText.value, JSON.stringify(currentItem));
+      list.innerHTML += '<li id= ' + inputText.value + '>' + inputText.value + '</li>';
+      inputText.value = '';
+      storageKey++;
+      // console.log(window.localStorage)  //for some reason my local storage values aren't printing to console??
+    } 
+  });
 
+
+  //check a grocery item off the list
+  list.addEventListener('dblclick', function(e) {
+    var clickedItem = e.target;
+    $(clickedItem).addClass('complete');
+    storageKeysForRemoval.push(e.target.id);
+
+    console.log(storageKeysForRemoval);
+
+  });
 
 
   // remove checked items from the list
   clearBtn.addEventListener('click', function() {
     $('.complete').remove();
+
+    for (var i = 0; i < storageKeysForRemoval.length; i++) {
+      localStorage.removeItem(storageKeysForRemoval[i])
+    }
+
+    storageKeysForRemoval = [];
+    console.log(storageKeysForRemoval)
   });
 
+
+
   //edit items on the list:
+  updateBtn.addEventListener('click', function(){
+  });
 
 
 });
+
+
 
 
